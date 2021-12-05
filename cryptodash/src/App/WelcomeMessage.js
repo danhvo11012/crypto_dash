c45341ebda0e20707fb9569b050b675c971d5059
Dan <danhvo11012@gmail.com>
2021-12-05 09:23:41 -0600

    Set global font

---
+++ cryptodash/src/App/WelcomeMessage.js
@@ -0,0 +1,6 @@
+function Welcome(props) {
+    return <h1>Welcome to CryptoDash</h1>
+}
+
+export default Welcome;
+  
--- cryptodash/src/App/index.js
+++ cryptodash/src/App/index.js
@@ -1,9 +1,16 @@
+import React, { Component } from 'react';
+import Welcome from './WelcomeMessage';
 import './App.css';
+import styled, { css } from 'styled-components';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Welcome  />
+      </div>
+    );  
+  }
 }
 
 export default App;
